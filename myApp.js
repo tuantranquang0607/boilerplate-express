6b23d082128bd2a3b553df8bd3f250c1a14a7888
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-16 10:30:41 -0300

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.get('/:word/echo', function(req, res) {
-  res.json({ echo: req.params.word });
+app.get('/name', function(req, res) {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
 });
+
 
 
 
