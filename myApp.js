15e3dfa8ca12f53bb6ab4534c8b5f6bb7b5e774e
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-16 10:24:54 -0300

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -2,24 +2,13 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-// console.log("Hello Word");
-
-// const path = __dirname + '/views/index.html';
-
-// app.get('/json', function(req, res) {
-//   let message = "Hello json";
-//   if (process.env.MESSAGE_STYLE === 'uppercase') {
-//     message = message.toUpperCase();
-//   }
-//   res.json({ message: message });
-// });
-
-// app.use('/public', express.static(__dirname + '/public'));
-
-app.use(function(req, res, next) {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
   next();
+}, function(req, res) {
+  res.json({ time: req.time });
 });
+
 
 
 
