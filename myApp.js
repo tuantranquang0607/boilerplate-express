a5d75e4f262b08a0980dc430aae7d4973eaa4a23
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-16 10:13:21 -0300

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello Word");
 
 const path = __dirname + '/views/index.html';
 
 app.get('/json', function(req, res) {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
 });
+
 
 app.use('/public', express.static(__dirname + '/public'));
 
