08da033b207f583428b4e67ef90bc9e91548cad4
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-16 10:22:20 -0300

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,20 +2,25 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-console.log("Hello Word");
+// console.log("Hello Word");
 
-const path = __dirname + '/views/index.html';
+// const path = __dirname + '/views/index.html';
 
-app.get('/json', function(req, res) {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
-  }
-  res.json({ message: message });
+// app.get('/json', function(req, res) {
+//   let message = "Hello json";
+//   if (process.env.MESSAGE_STYLE === 'uppercase') {
+//     message = message.toUpperCase();
+//   }
+//   res.json({ message: message });
+// });
+
+// app.use('/public', express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
 });
 
-
-app.use('/public', express.static(__dirname + '/public'));
 
 
 
