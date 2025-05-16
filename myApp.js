ed971f98987e38a368c909183daa4ad77833db88
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-16 10:51:16 -0300

    Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -2,8 +2,20 @@ let express = require('express');
 let app = express();
 
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.route('/name')
+  .get(function(req, res) {
+    const firstName = req.query.first;
+    const lastName = req.query.last;
+    res.json({ name: `${firstName} ${lastName}` });
+  })
+  .post(function(req, res) {
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    res.json({ name: `${firstName} ${lastName}` });
+  });
+
 
 
 
